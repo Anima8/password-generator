50a20ca738e07982dd3e1f4d8275b366037f4135
Anima8 <68305643+Anima8@users.noreply.github.com>
2020-09-03 10:07:19 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,13 @@ setup(
     version='1.0',
     description='passwordgeneratorTool',
     author='Anima8',
-    url='https://github.com/Anima8/passwordgenerator',
+    url='https://github.com/Anima8/password-generator',
     license='MIT',
     python_requires='>=3.4',
     py_modules=['passwordgenerator'],
-    entry_points={'console_scripts': ['passwordgenerator=passwordgenerator:main',]},
+    entry_points={
+        'console_scripts': [
+            'passwordgenerator=passwordgenerator:main',
+        ]
+    },
 )
